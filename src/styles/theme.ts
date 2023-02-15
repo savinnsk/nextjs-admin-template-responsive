08d884919f0b5a908ed3a5c13e4f24c754a11a76
savinnsk <savio.document@gmail.com>
2023-02-15 14:04:45 -0300

    bolirplate done

--- src/styles/theme.ts
+++ src/styles/theme.ts
@@ -1,12 +1,31 @@
-import { extendTheme } from "@chakra-ui/react";
+import { extendTheme, Theme } from "@chakra-ui/react";
 
-export const theme = extendTheme({
+const customTheme = {
+  fonts: {
+    heading: "Roboto",
+    body: "Roboto",
+  },
   styles: {
-    global: {
-      body: {
-        bg: "gray.900",
-        color: "gray.50",
-      },
+    body: {
+      bg: "gray.900",
+      font: "gray.50",
+    },
+    global: {},
+  },
+  colors: {
+    gray: {
+      "50": "#EEEEF2",
+      "100": "#D1D2DC",
+      "200": "#B3B5C6",
+      "300": "#9699B0",
+      "400": "#797D9A",
+      "500": "#616480",
+      "600": "#4B4D63",
+      "700": "#353646",
+      "800": "#1F2029",
+      "900": "#181B23",
     },
   },
-});
+} as Theme;
+
+export const theme = extendTheme(customTheme);
