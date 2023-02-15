9afe13577ce905073ac4cf93815196c0345623d7
savinnsk <savio.document@gmail.com>
2023-02-15 15:03:04 -0300

    feat: sign in component

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,67 @@
+import {
+  Button,
+  Flex,
+  Input,
+  Stack,
+  FormLabel,
+  FormControl,
+} from "@chakra-ui/react";
+
 export default function Home() {
   return (
-    <>
-      <h1>dash</h1>
-    </>
+    <Flex
+      w="100vw"
+      h="100vh"
+      align="center"
+      justify="center"
+      bg="gray.900"
+      color="gray.50"
+    >
+      <Flex
+        as="form"
+        w="100%"
+        maxW="360"
+        bg="gray.800"
+        p="8"
+        borderRadius={8}
+        flexDir="column"
+      >
+        <Stack spacing="4">
+          <FormControl>
+            <FormLabel htmlFor="email">E-mail</FormLabel>
+            <Input
+              id="email"
+              name="email"
+              type="email"
+              focusBorderColor="pink.500"
+              bgColor="gray.900"
+              variant="filled"
+              _hover={{
+                bgColor: "gray.900",
+              }}
+              size="lg"
+            />
+
+            <FormLabel htmlFor="password">Password</FormLabel>
+            <Input
+              id="password"
+              name="password"
+              type="password"
+              focusBorderColor="pink.500"
+              bgColor="gray.900"
+              variant="filled"
+              _hover={{
+                bgColor: "gray.900",
+              }}
+              size="lg"
+            />
+          </FormControl>
+        </Stack>
+
+        <Button type="submit" mt="6" colorScheme="pink" size="lg">
+          Entrar
+        </Button>
+      </Flex>
+    </Flex>
   );
 }
