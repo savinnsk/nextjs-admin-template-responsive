f84d9c62e53b34a98d740b430ac68bc14b508b20
savinnsk <savio.document@gmail.com>
2023-04-01 16:29:46 -0300

    feat: add grid to show info

--- src/pages/dashboard.tsx
+++ src/pages/dashboard.tsx
@@ -1,6 +1,10 @@
+import dynamic from "next/dynamic";
+
+import { Box, Flex, SimpleGrid, Text, theme } from "@chakra-ui/react";
+
+import { Chart } from "@/components/chart";
 import { Header } from "@/components/header";
 import { Sidebar } from "@/components/sidebar";
-import { Flex } from "@chakra-ui/react";
 
 export default function Dashboard() {
   return (
@@ -9,6 +13,30 @@ export default function Dashboard() {
 
       <Flex w="100%" my="6" maxWidth={1480} mx="auto" px="6">
         <Sidebar />
+        {
+          //flex 1 to fill the width aside sidebar
+          //SimpleGrid automatize the responsively
+        }
+        <SimpleGrid
+          flex="1"
+          gap="4"
+          minChildWidth="320px"
+          alignItems="flex-start"
+        >
+          <Box p="4" bg="gray.800" borderRadius={8}>
+            <Text fontSize="lg" mb="4">
+              new accounts this week
+            </Text>
+            <Chart type="area" height={160} width={"100%"} />
+          </Box>
+
+          <Box p="4" bg="gray.800" borderRadius={8}>
+            <Text fontSize="lg" mb="4">
+              info
+            </Text>
+            <Chart type="area" height={160} width={"100%"} />
+          </Box>
+        </SimpleGrid>
       </Flex>
     </Flex>
   );
