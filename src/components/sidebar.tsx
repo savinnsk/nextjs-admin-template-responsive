8506edd6b315202a64a2daa23e82bde86c75994a
savinnsk <savio.document@gmail.com>
2023-04-03 12:07:02 -0300

    refactor: sidebar move Section and Links to their component

--- src/components/sidebar.tsx
+++ src/components/sidebar.tsx
@@ -5,54 +5,22 @@ import {
   RiGitMergeLine,
   RiInputMethodLine,
 } from "react-icons/ri";
+import { NavSection } from "./SideBar/navSection";
+import { NavLink } from "./SideBar/navLink";
 
 export function Sidebar() {
   return (
     <Box as="aside" w="64" mr="8">
       <Stack spacing="12" align="flex-start">
-        <Box>
-          <Text fontWeight="bold" color="gray.400" fontSize="small">
-            General
-          </Text>
+        <NavSection title="General">
+          <NavLink icon={RiDashboardLine}>Dashboard</NavLink>
+          <NavLink icon={RiContactsLine}>Users</NavLink>
+        </NavSection>
 
-          <Stack spacing="4" mt="8" align="stretch">
-            <Link display="flex" alignItems="center">
-              <Icon as={RiDashboardLine} fontSize="20" />
-              <Text ml="4" fontWeight="medium">
-                DashBoard
-              </Text>
-            </Link>
-
-            <Link display="flex" alignItems="center">
-              <Icon as={RiContactsLine} fontSize="20" />
-              <Text ml="4" fontWeight="medium">
-                Users
-              </Text>
-            </Link>
-          </Stack>
-        </Box>
-
-        <Box>
-          <Text fontWeight="bold" color="gray.400" fontSize="small">
-            Automatization
-          </Text>
-
-          <Stack spacing="4" mt="8" align="stretch">
-            <Link display="flex" alignItems="center">
-              <Icon as={RiInputMethodLine} fontSize="20" />
-              <Text ml="4" fontWeight="medium">
-                Graph
-              </Text>
-            </Link>
-
-            <Link display="flex" alignItems="center">
-              <Icon as={RiGitMergeLine} fontSize="20" />
-              <Text ml="4" fontWeight="medium">
-                Automatization
-              </Text>
-            </Link>
-          </Stack>
-        </Box>
+        <NavSection title=" Automatization">
+          <NavLink icon={RiInputMethodLine}>Graph</NavLink>
+          <NavLink icon={RiGitMergeLine}>Automatization</NavLink>
+        </NavSection>
       </Stack>
     </Box>
   );
