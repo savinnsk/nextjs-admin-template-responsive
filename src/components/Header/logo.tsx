20bcfd2c1a7dadd5247fa038eedb27a278e86e7b
savinnsk <savio.document@gmail.com>
2023-04-03 11:37:30 -0300

    move logo to a component

---
+++ src/components/Header/logo.tsx
@@ -0,0 +1,12 @@
+import { Flex, Text } from "@chakra-ui/react";
+
+export function Logo() {
+  return (
+    <Text fontSize="3xl" fontWeight="bold" letterSpacing="tight" w="64">
+      AdminPanel
+      <Text as="span" ml="1" color="pink.500">
+        .
+      </Text>
+    </Text>
+  );
+}
