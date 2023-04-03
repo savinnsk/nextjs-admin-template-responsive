ac6d63c7f4d54d7eee93fb011c7b38ebe32e6ffa
savinnsk <savio.document@gmail.com>
2023-04-03 12:05:33 -0300

    refactor: move navLink to a component

---
+++ src/components/SideBar/navLink.tsx
@@ -0,0 +1,24 @@
+import {
+  Text,
+  Link,
+  Icon,
+  LinkProps as ChakraLinkProps,
+} from "@chakra-ui/react";
+import { ElementType } from "react";
+
+interface NavLinkProps extends ChakraLinkProps {
+  // element type name of the component
+  icon: ElementType;
+  children: string;
+}
+
+export function NavLink({ icon, children, ...rest }: NavLinkProps) {
+  return (
+    <Link display="flex" alignItems="center" {...rest}>
+      <Icon as={icon} fontSize="20" />
+      <Text ml="4" fontWeight="medium">
+        {children}
+      </Text>
+    </Link>
+  );
+}
